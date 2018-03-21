c1ffc211f6072d7979fa49ba683ae0ab71a955da
Adam Mrachek <amrachek@outlook.com>
2018-03-21 02:39:08 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,1 +1,9 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kit kat', 'skittles'];
+
+function addElementToBeginningOfArray(arr, el) {
+   return arr.unshift(el);
+}
+
+function destructivelyAddElementToBeginningOfArray(arr, el) {
+  arr.unshift(el);
+}

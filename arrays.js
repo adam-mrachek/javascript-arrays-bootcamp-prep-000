5482e878ea7aabf3b5a7fe3aa65872f9d59f5e28
Adam Mrachek <amrachek@outlook.com>
2018-03-21 02:48:30 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,7 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kit kat', 'skittles'];
 
 function addElementToBeginningOfArray(arr, el) {
-   return arr.unshift(el);
+   return [el, ...arr];
 }
 
 function destructivelyAddElementToBeginningOfArray(arr, el) {
